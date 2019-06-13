a4be5e6c0e83101026b972bd1a84048980a87c67
rowneee <roni.shabo@gmail.com>
2019-06-13 17:12:42 -0400

    Working Separate Index For Each Slider

--- my-project-client/src/Profile/SliderComponent.js
+++ my-project-client/src/Profile/SliderComponent.js
@@ -4,28 +4,77 @@ import LeftArrow from './LeftArrow'
 import RightArrow from './RightArrow'
 
 class SliderComponent extends React.Component {
+  state = {
+    currentIndex: 0,
+    translateValue: 0
+  }
 
+  goToPrevSlide = () => {
+   if(this.state.currentIndex === 0)
+     return;
+
+   this.setState(prevState => ({
+     currentIndex: prevState.currentIndex - 1,
+     translateValue: prevState.translateValue + this.slideWidth()
+   }))
+ }
+
+ goToNextSlide = (array) => {
+   // Exiting the method early if we are at the end of the images array.
+   // We also want to reset currentIndex and translateValue, so we return
+   // to the first image in the array.
+   if(this.state.currentIndex === array.length - 1) {
+     return this.setState({
+       currentIndex: 0,
+       translateValue: 0
+     })
+   }
+
+   // This will not run if we met the if condition above
+   this.setState(prevState => ({
+     currentIndex: prevState.currentIndex + 1,
+     translateValue: prevState.translateValue + -(this.slideWidth())
+   }));
+ }
+
+ slideWidth = () => {
+    console.log(document.querySelector('.slide').clientWidth)
+    return document.querySelector('.slide').clientWidth
+
+ }
+
+  // currentIndex={this.state.currentIndex}
+  // translateValue={this.state.translateValue}
+  // goToPrevSlide={this.goToPrevSlide}
+  // goToNextSlide={this.goToNextSlide}
+  // items={this.props.items}
+  // borrowedItems={this.props.borrowedItems}
+  // myItems={this.props.myItems}
+  // accessories={this.props.accessories}
+  // tops={this.props.tops}
+  // bottoms={this.props.bottoms}
+  // shoes={this.props.shoes}
   render() {
     return (
       <div>
         <div className="slider">
           <div className="slider-wrapper"
             style={{
-              transform: `translateX(${this.props.translateValue}px)`,
+              transform: `translateX(${this.state.translateValue}px)`,
               transition: 'transform ease-out 0.45s'
             }}>
               {
-                this.props.accessories.map((item, i) => (
+                this.props.category.map((item, i) => (
                   <Slide key={i} item={item} />
                 ))
               }
           </div>
           <LeftArrow
-           goToPrevSlide={()=>this.goToPrevSlide(this.props.currentIndex, this.props.translateValue)}
+           goToPrevSlide={this.goToPrevSlide}
           />
 
           <RightArrow
-           goToNextSlide={()=>this.goToNextSlide(this.props.accessories, this.props.currentIndex, this.props.translateValue)}
+           goToNextSlide={()=>this.goToNextSlide(this.props.category)}
           />
       </div>
       </div>
--- my-project-client/src/Profile/SliderContainer.js
+++ my-project-client/src/Profile/SliderContainer.js
@@ -4,59 +4,23 @@ import {Container} from 'semantic-ui-react'
 
 export default class SliderContainer extends Component {
 
-  state = {
-    currentIndex: 0,
-    translateValue: 0,
-  }
-
-  goToPrevSlide = () => {
-   if(this.state.currentIndex === 0)
-     return;
-
-   this.setState(prevState => ({
-     currentIndex: prevState.currentIndex - 1,
-     translateValue: prevState.translateValue + this.slideWidth()
-   }))
- }
-
- goToNextSlide = (array) => {
-   // Exiting the method early if we are at the end of the images array.
-   // We also want to reset currentIndex and translateValue, so we return
-   // to the first image in the array.
-   if(this.state.currentIndex === array.length - 1) {
-     return this.setState({
-       currentIndex: 0,
-       translateValue: 0
-     })
-   }
-
-   // This will not run if we met the if condition above
-   this.setState(prevState => ({
-     currentIndex: prevState.currentIndex + 1,
-     translateValue: prevState.translateValue + -(this.slideWidth())
-   }));
- }
-
- slideWidth = () => {
-    console.log(document.querySelector('.slide').clientWidth)
-    return document.querySelector('.slide').clientWidth
-
- }
-
  render() {
    return (
     <div>
       <SliderComponent
-        currentIndex={this.state.currentIndex}
-        translateValue={this.state.translateValue}
-        goToPrevSlide={this.goToPrevSlide}
-        goToNextSlide={this.goToNextSlide}
-        items={this.props.items}
-        borrowedItems={this.props.borrowedItems}
-        myItems={this.props.myItems}
+        category={this.props.accessories}
         accessories={this.props.accessories}
+      />
+      <SliderComponent
+        category={this.props.tops}
         tops={this.props.tops}
+      />
+      <SliderComponent
+        category={this.props.bottoms}
         bottoms={this.props.bottoms}
+      />
+      <SliderComponent
+        category={this.props.shoes}
         shoes={this.props.shoes}
       />
    </div>
